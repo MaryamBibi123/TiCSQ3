dea2667b22cb6c0999aa05bd73e0b08d5a7d2c51
Maryam Bibi <102379865+MaryamBibi123@users.noreply.github.com>
2024-11-18 14:26:15 +0500

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,1 +1,13 @@
-System.out.println("Hello Jenkins");
+public class SimpleSum {
+    public static void main(String[] args) {
+        // Declare two numbers
+        int num1 = 10;
+        int num2 = 20;
+
+        // Calculate the sum
+        int sum = num1 + num2;
+
+        // Display the result
+        System.out.println("The sum of " + num1 + " and " + num2 + " is: " + sum);
+    }
+}
