55776fbd5ad2cbc2b6e5103d5ad8602ef9aa0695
Maryam Bibi <102379865+MaryamBibi123@users.noreply.github.com>
2024-12-06 11:21:02 +0500

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -6,7 +6,6 @@ public class Hello {
 
         // Calculate the sum
         int sum = num1 + num2;
-
         // Display the result
         System.out.println("The sum of " + num1 + " and " + num2 + " is: " + sum);
         
